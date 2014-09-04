8bff33a2150a3408a010892e4a9883f33f2286cb
Matthew Lorentz <matt@lorentz.is>
2014-09-04 14:52:19 +0000

    Added ability to change port with environment variable

--- peeping-pi.py
+++ peeping-pi.py
@@ -6,11 +6,11 @@ import os, ctypes
 def main():
     shared_occupancy_state = Value(ctypes.c_bool, False)
     eventq = Queue()
+    port = int(os.getenv('PEEPING_PI_PORT', 80))
     sensor = Process(target=models.sensor.run, args=(eventq,))
     accumulator = Process(target=models.accumulator.run, args=(eventq, shared_occupancy_state))
-    server = Process(target=webserver.run, args=(shared_occupancy_state,))
+    server = Process(target=webserver.run, args=(shared_occupancy_state, port))
 
-    parent = os.getpid()
     sensor.start()
     accumulator.start()
     server.start()
